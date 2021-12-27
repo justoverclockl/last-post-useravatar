88e51e25e5987dd733fd402eb71060136f3b6484
Marco Colia <79002016+justoverclockl@users.noreply.github.com>
2021-12-27 16:43:39 +0100

    username added

--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -7,14 +7,16 @@ import Link from 'flarum/common/components/Link';
 app.initializers.add('justoverclock/last-post-useravatar', () => {
   extend(DiscussionListItem.prototype, 'infoItems', function (items) {
     const discussion = this.attrs.discussion;
+    console.log(discussion)
     const lastPostedUser = discussion.lastPostedUser();
+    const lastPostedUserName = discussion.lastPostedUser().displayName()
 
     items.add(
       'lastPostedUserAvatar',
       <Link
         className="DiscussionListItem-author"
         href={lastPostedUser ? app.route.user(lastPostedUser) : '#'}
-        title={app.translator.trans('justoverclock-last-post-useravatar.forum.lastPostedUser')}>
+        title={app.translator.trans('justoverclock-last-post-useravatar.forum.lastPostedUser') + lastPostedUserName}>
         {avatar(lastPostedUser, { title: '', className: 'lastPostedUserAvatar' })}
       </Link>
     );
